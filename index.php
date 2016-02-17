70fffba0db81b0e661d9e80b4ff5a2a6b715acd8
JessLee5 <jess32@gmx.net>
2016-02-17 13:35:40 +0100

    Wieder die Header.php hergestellt

--- index.php
+++ index.php
@@ -1,30 +1,6 @@
-<html>
-    <head>
-    <link rel="stylesheet" href="css/style.css">
-	<meta http-equiv="Cache-Control" content="max-age=3600, public">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Das Zocker-Wonderland</title>
-	<meta name="Content-Language" content="de" />
-	<meta name="language" content="Deutsch" />
-	<meta name="description" content="Die neusten Infos zu alles Games!" />
-	<meta name="author" content="Jessica Lee Schulz, Jasmin Schermuly, Enrico Gette">
-	<meta name="keywords" content="Games, neue Spiele, Zocken, spielen, PC, Konsolen, Genre">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	<link rel="shortcut icon" href="../img/favicon.ico" type="image/vnd.microsoft.icon">
-	<link rel="icon" href="../img/favicon.ico" type="image/vnd.microsoft.icon">
-	<link rel="stylesheet" href="../css/style.css">
-</head>
-    <body>
-    <main>
-        <?php
-        include('header.php');
-        include('navi.php'); 
-        include('content.php');
-        ?>
-    </main>
-        <?php
-            include('footer.php');
-        ?>
-    </body>
-</html>
-        
+<?php 
+include('header.php');
+include('navi.php'); 
+include('content.php');
+include('footer.php');
+?>
