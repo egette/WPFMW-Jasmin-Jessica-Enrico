f8c2eba0e1eecb523821f8d99f876cb25946d492
JessLee5 <jess32@gmx.net>
2016-02-20 14:34:04 +0100

    Revert "so besser? :D"
    This reverts commit da334e1bab40675538f6e0362da8250b95c3d7f6.

--- header.php
+++ header.php
@@ -3,14 +3,22 @@ include('db.php');
 ?>
 <!DOCTYPE html>
 <html lang="de">
+<head>
+	<meta http-equiv="Cache-Control" content="max-age=3600, public">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Die Zock3r-W0nd3rland</title>
+	<meta name="Content-Language" content="de" />
+	<meta name="language" content="Deutsch" />
+	<meta name="description" content="Die neusten Infos zu alles Games!" />
+	<meta name="author" content="Jessica Lee Schulz, Jasmin Schermuly, Enrico Gette">
+	<meta name="keywords" content="Games, neue Spiele, Zocken, spielen, PC, Konsolen, Genre">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="shortcut icon" href="../img/favicon.ico" type="image/vnd.microsoft.icon">
+	<link rel="icon" href="../img/favicon.ico" type="image/vnd.microsoft.icon">
+	<link rel="stylesheet" href="../css/style.css">
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+</head>
 <body>
 	<header>
-        <div class="head">
-            <div id="logo">
-		<a href="."><img src="../img/logo-ganz.png" alt="Logo von Zocker-Wonderland" /></a></div>
-        <form>
-            <label for="bname">Spiel suchen</label> 
-    <input type="text" id="search" size="30" maxlength="30">
-        </form>
-        </div>
+		<a href="."><img src="../img/logo-ganz.png" alt="Logo von Zock3r-W0nd3rland" /></a>
 	</header>
--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<html>
-    <head>
-    </head>
-    <body>
-    <main>
-        <?php
-        include('header.php');
-        include('navi.php'); 
-        include('content.php');
-        ?>
-    </main>
-        <?php
-            include('footer.php');
-        ?>
-    </body>
-</html>
-        
+<?php 
+include('header.php');
+include('navi.php'); 
+include('content.php');
+include('footer.php');
+?>
