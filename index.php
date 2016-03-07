781ab468d8950c19c09fcba3c8717b5fb2f69c31
JessLee5 <jess32@gmx.net>
2016-03-07 10:53:35 +0100

    index

--- index.php
+++ index.php
@@ -2,22 +2,6 @@
 session_start();
 include('header.php');
 include('navi.php');
-include('search.php'); 
-
-//Wenn nicht eingeloggt, dann login formular und registrierungslinks anzeigen
-if(!isset($_SESSION['userid'])) {
-	?>
-	<br><a href="registrieren.php" id="regi"> Noch nicht registiert? </a><br>
-	<?php	
-		include('login.php');
-}
-//wenn eingeloggt, dann link zum ausloggen anzeigen
-if(isset($_SESSION['userid'])) {
-	?>
-	<br><a href="logout.php" id="logout"> Ausloggen ? </a><br>
-	<?php	
-}
-
 include('content.php');
 include('footer.php');
 ?>
